d33fddbff893a4a37bae1f49550ada38e5443fe7
apcnkye_7 <hammondgabriel4@gmail.com>
2024-12-19 12:48:02 +0000

    Deleted comments

--- resources/azurerm_monitor_diagnostic_setting/main.tf
+++ resources/azurerm_monitor_diagnostic_setting/main.tf
@@ -1,16 +1,3 @@
-# resource "azurerm_monitor_diagnostic_setting" "cf_azurerm_monitor_diagnostic_setting" {
-#   name               = "${var.name}"
-#   target_resource_id = "${var.target_resource_id}"
-#   storage_account_id = "${var.storage_account_id}"
-
-#   enabled_log {
-#     category = "${var.enabled_log_category}"
-#   }
-
-#   metric {
-#     category = "${var.metric_category}"
-#   }
-# }
 
 resource "azurerm_monitor_diagnostic_setting" "storage_diagnostics" {
   name                       = "${var.name}"
